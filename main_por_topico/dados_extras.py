23dac1d3382f241870ba85f8f26a4ddcf94da43d
Devison <devison_santos@yahoo.com.br>
2023-07-26 19:42:27 -0300

    [Tutorial] Tipos de dados extras

---
+++ main_por_topico/dados_extras.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta, time
+from typing import Union
+from uuid import UUID
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+
+@app.put("/items/{item_id}")
+async def read_item(
+        item_id: int,
+        start_datetime: Union[datetime, None] = Body(default=None),
+        end_datetime: Union[datetime, None] = Body(default=None),
+        repeat_at: Union[time, None] = Body(default=None),
+        process_after: Union[timedelta, None] = Body(default=None)
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration
+    }
